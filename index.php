91fe221962fb9ad65444978c4a93fc7855bc0ed2
Imam Yudhi Pradana <38575733+imamyudhipradana@users.noreply.github.com>
2019-05-25 15:13:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
 			<ul class="navbar-nav mr-auto">
 			<li class="nav-item active">
-				<a class="nav-link" href="https://smart-parkir.azurewebsites.net/">Home</a>
+				<a class="nav-link" href="https://submisi.azurewebsites.net/">Home</a>
 			</li>
 			
 		</div>
