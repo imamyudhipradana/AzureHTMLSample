1a68cfcd3b92b3b401a12def2469bcd4e1214217
Imam Yudhi Pradana <38575733+imamyudhipradana@users.noreply.github.com>
2019-05-25 14:13:32 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:submisi.database.windows.net,1433; Database = blog_crud", "adminserver", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:submisi.database.windows.net,1433; Database = blog_crud", "adminserver", "@dmin123456789");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -9,6 +9,6 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "adminserver@submisi", "pwd" => "{your_password_here}", "Database" => "blog_crud", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "adminserver@submisi", "pwd" => "@dmin123456789", "Database" => "blog_crud", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:submisi.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
