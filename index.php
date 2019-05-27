0b9c61a10ddd0de62e4b218c65f3a3746addff38
Imam Yudhi Pradana <38575733+imamyudhipradana@users.noreply.github.com>
2019-05-27 16:01:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
             $npk = $_POST['npk'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO daftar (nama, nik, kyw, date) 
+            $sql_insert = "INSERT INTO daftar (nama, nim, npk, date) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
