f9dac539806ddc5302c82dbba42062b926bfde6c
Imam Yudhi Pradana <38575733+imamyudhipradana@users.noreply.github.com>
2019-05-27 16:23:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,38 +2,13 @@
 <html>
  <head>
  <meta charset="utf-8">
-<!--     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> -->
-<!--     <meta name="description" content="">
-    <meta name="author" content=""> -->
-
     <title>Data Kendaraan Bermotor PT. Indofood(Branch Bandung)</title>
-
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
-
-<!--     Bootstrap core CSS
-    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
-
-<!--     <!-- Custom styles for this template -->
-<!--     <link href="starter-template.css" rel="stylesheet"> --> 
   </head>
  <body>
-<!--  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button> -->
-<!-- 		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
-			<ul class="navbar-nav mr-auto"> -->
-<!-- 			<li class="nav-item active">
-				<a class="nav-link" href="https://submisi.azurewebsites.net/">Home</a>
-			</li> -->
-			
-<!-- 		</div> -->
-<!-- 		</nav>
- -->
     <main role="main" class="container">
     <div class="starter-template"> <br><br><br>
         <h1>Data Kendaraan Bermotor PT. Indofood</h1>
-<!--         <p class="lead">Isikan dengan lengkap dari <b>Nama, NIK, Posisi Pekerjaan </b> anda.<br> Kemudian Click <b>Submit Data Karyawan</b> untuk Registrasi Kendaraan anda.</p> <br> -->
         <span class="border-top my-3"></span>
       </div>
         <form action="index.php" method="POST">
@@ -121,8 +96,7 @@
 </tbody>
 </table>
  
-<!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
     <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
     <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
