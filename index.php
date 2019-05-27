942018403864566b045aeb1ecb7f26215c98d475
Imam Yudhi Pradana <38575733+imamyudhipradana@users.noreply.github.com>
2019-05-27 16:17:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,13 +21,13 @@
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
-			<ul class="navbar-nav mr-auto">
+<!-- 		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+			<ul class="navbar-nav mr-auto"> -->
 <!-- 			<li class="nav-item active">
 				<a class="nav-link" href="https://submisi.azurewebsites.net/">Home</a>
 			</li> -->
 			
-		</div>
+<!-- 		</div> -->
 		</nav>
 
     <main role="main" class="container">
